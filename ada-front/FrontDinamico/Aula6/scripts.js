660fdf71025348eaf87b3709fdcf47773c876127
eduardo.murakoshi@groove.tech <eduardo.murakoshi@groove.tech>
2023-03-25 12:10:54 -0300

    Formulario criado

---
+++ ada-front/FrontDinamico/Aula6/scripts.js
@@ -0,0 +1,20 @@
+// https://github.com/dkayke-aulas/agenda-contatos-backend
+/*
+const dados = {
+    nome: "Ada Lovelace",
+    email: "ada@email.com,
+    telefones: [
+        { 
+            numero: "(11) 99123-4567",
+            tipo: "casa"
+        }
+    ],
+    endereco: {
+        logradouro: "Rua das programadoras",
+        cidade: "Vale do silício",
+        estado: "Codefornia",
+        cep: "10100-100",
+        pais: "Programaland"
+    }
+}
+*/
