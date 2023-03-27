c5bd234ef669d8a50056d4bcbd27a5766ed7aabd
eduardo.murakoshi@groove.tech <eduardo.murakoshi@groove.tech>
2023-03-27 00:38:06 -0300

    Exercicio para ser entregue segunda

--- ada-front/FrontDinamico/Aula6/scripts.js
+++ ada-front/FrontDinamico/Aula6/scripts.js
@@ -1,12 +1,11 @@
 // https://github.com/dkayke-aulas/agenda-contatos-backend
-/*
 const dados = {
     nome: "Ada Lovelace",
-    email: "ada@email.com,
+    email: "ada@email.com",
     telefones: [
         { 
             numero: "(11) 99123-4567",
-            tipo: "casa"
+            tipo: "celular"
         }
     ],
     endereco: {
@@ -17,4 +16,18 @@ const dados = {
         pais: "Programaland"
     }
 }
-*/
+
+const inputs = document.getElementsByClassName('texto')
+
+inputs[0].value = dados.nome
+inputs[1].value = dados.email
+inputs[2].value = dados.telefones[0].numero
+inputs[3].value = dados.endereco.logradouro
+inputs[4].value = dados.endereco.cidade
+inputs[5].value = dados.endereco.estado
+inputs[6].value = dados.endereco.cep
+inputs[7].value = dados.endereco.pais
+
+inputs[0].addEventListener("change", function() {
+    console.log(inputs[0].value)
+})
